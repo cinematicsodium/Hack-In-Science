305a2bb154557768e48c5c0365b2f02f401a249f
cinematicsodium <126626502+cinematicsodium@users.noreply.github.com>
2024-02-21 13:47:15 -0700

    Update romanNum.py

--- romanNum.py
+++ romanNum.py
@@ -1,23 +1,70 @@
-'''
-Convert a Roman numeral to an integer.
-'''
+import time
+from time import sleep
 
-def from_roman_numeral():
-    roman = input("\nEnter a roman numeral: ")
+def roman_to_int():
+  sleep(1)
+  print('Enter a Roman numeral to an integer.')
+  print()
+  sleep(0.5)
+  print('Example:')
+  sleep(0.5)
+  print(f'Roman numeral:\t MCMXCVIII')
+  sleep(0.5)
+  print(f'Integer:\t\t 1998')
+  print()
+  sleep(0.5)
 
-    val = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+  input_numerals: str = str(input(f'Roman numeral:\t ')).upper().strip()
+  sleep(1)
 
-    sum = 0
-    prev_val = 0
+  # Validate Characters
+  while True:
+    try:
+      # Exit
+      if input_numerals.lower() == 'q':
+        print('Goodbye!')
+        exit()
+      # Invalid Character
+      else:
+        for i in input_numerals:
+          if i.isdigit() or i not in "IVXLCDM":
+            print(f'Invalid character: {i}')
+            sleep(1)
+            print()
+            return ValueError
+    except ValueError:
+      print('Invalid entry.')
+      sleep(1)
+      continue
+    # Valid character
+    else:
+      break
 
-    for i in roman:
-        curr_val = val[i]
-        if curr_val > prev_val:
-            sum += curr_val - 2 * prev_val
-        else:
-            sum += curr_val
-        prev_val = curr_val
+  # Define numerical values
+  val: dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
-    print(f'\n{sum}\n')
+  # Convert numerals to integers
+  numerical_values: list = [int(val[i]) for i in input_numerals]
 
-from_roman_numeral()
+  sum = 0
+
+  x = 0
+  while x < len(numerical_values):
+    if x == 0:
+      sum += numerical_values[x]
+      x += 1
+    elif numerical_values[x] > numerical_values[x - 1]:
+      sum += numerical_values[x]
+      sum -= 2 * numerical_values[x - 1]
+      x += 1
+    else:
+      sum += numerical_values[x]
+      x += 1
+
+  print(f'Integer:\t\t {sum}')
+  print()
+  sleep(0.5)
+
+
+while True:
+  roman_to_int()
