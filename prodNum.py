398a9aa1a3fed4c9c57fd869a07c3d7770f92eb6
cinematicsodium <126626502+cinematicsodium@users.noreply.github.com>
2024-02-21 16:24:15 -0700

    Update prodNum.py

--- prodNum.py
+++ prodNum.py
@@ -1,19 +1,22 @@
 '''
-Create a function to find the product of a list of numbers
+Create a function to find the product of a random list of integers.
 '''
 
 from random import randint
 
 def mul(numbers: list):
     prod: int = 1
+    count = 0
     for i in numbers:
         prod *= i
+        count += 1
     
-    formatted_numbers: str = ', '.join(str(num) for num in sorted(numbers))
+    formatted_integers: str = ', '.join(str(num) for num in sorted(numbers))
 
-    print(f'Numbers to multiply: {formatted_numbers}')
-    print(f'Product: {prod}')
+    print(f'Integers to multiply: {formatted_integers} (Count: {count})\n')
+    print(f'Product of integers: {prod}')
 
-listNums: list = [randint(1,25) for i in range(5)]
+
+listNums: list = [randint(1,25) for i in range(randint(5,20))]
 
 mul(listNums)
