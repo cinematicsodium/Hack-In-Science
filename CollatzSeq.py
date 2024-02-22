aee9ba13d4822208f5e4cf2574eff62fe39bb675
Name <no@email.com>
2024-02-21 22:11:05 -0700

    Update CollatzSeq.py

--- CollatzSeq.py
+++ CollatzSeq.py
@@ -9,11 +9,27 @@ Collatz sequence: A mathematical sequence that starts with a positive integer.
 Example: 6
     6 -> 3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
 
-It is believed that no matter which positive integer you start with, the sequence will always reach 1. However, this has not been proven yet and remains an unsolved problem in mathematics.
+It is believed that no matter which positive integer you start with, 
+the sequence will always reach 1. However, this has not been proven 
+yet and remains an unsolved problem in mathematics.
 '''
-
+from time import sleep
 def collatz_length():
-    n: int = int(input('\nEnter a number to display its Collatz sequence: '))
+    while True:
+        sleep(0.5)
+        n: str = str(input('\nEnter a number from 1 - 256 to display its Collatz sequence ("q" to quit): '))
+        if n.lower() == 'q':
+            sleep(1)
+            print(f'\nGoodbye!\n')
+            sleep(1)
+            exit()
+        elif not n.isdigit() or int(n) > 256:
+            sleep(1)
+            print(f'\nInvalid Entry. Please try again.')
+            continue
+        else:
+            break
+    n = int(n)
     collatzSeq = [n]
     lenSequence = 0
     startNum = n
@@ -24,9 +40,13 @@ def collatz_length():
             n = n * 3 + 1
         lenSequence += 1
         collatzSeq.append(n)
+    collatzSeq = ', '.join(str(i) for i in collatzSeq)
+    sleep(1)
+    print(f'\nNumber:\t\t{startNum}')
+    sleep(0.5)
+    print(f'Sequence:\t{collatzSeq}')
+    sleep(0.5)
+    print(f'Seq. Count:\t{lenSequence} numbers\n')
 
-    print(f'\n Number:\t{startNum} \n Sequence:\t{collatzSeq} \n Count:\t\t{lenSequence} numbers\n')
-
-
-collatz_length()
-
+while True:
+    collatz_length()
